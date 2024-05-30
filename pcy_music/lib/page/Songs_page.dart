7ebc6d48f78ec6519e36d2b03922ecd4a674d823
roshan pcy <roshan@gmail.com>
2024-05-30 08:24:34 +0530

    added images on song file

--- pcy_music/lib/page/Songs_page.dart
+++ pcy_music/lib/page/Songs_page.dart
@@ -12,18 +12,19 @@ class SongsPage extends StatelessWidget {
         return SafeArea(
           child: Scaffold(
               backgroundColor: Theme.of(context).colorScheme.surface,
-              body: const Padding(
-                padding: EdgeInsets.only(left: 10, right: 10, top: 10),
+              body: Padding(
+                padding: const EdgeInsets.only(left: 10, right: 10, top: 10),
                 child: Column(
                   children: [
-                    Row(
+                    const Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Icon(Icons.arrow_back),
                         Text('P L A  Y  L I S T'),
                         Icon(Icons.menu),
                       ],
-                    )
+                    ),
+                    NeoBox(child: Image.asset('assets/images/img1.jpg'))
                   ],
                 ),
               )),
